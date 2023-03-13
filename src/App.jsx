16b5c4275c7f85fdc49a92e3460394d1a74f0993
ramosaqueda <ramos.aqueda@gmail.com>
2023-03-13 16:14:30 -0300

    corregido

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@
 import Header  from './components/AppBar/Header'
 import Finder from './components/Finder/Finder'
 import './App.css'
-import Carga from './components/Carga'
+ 
 
 function App() { 
   return (
     <div className="App">
      <Header />
-    <Carga />
+     <Finder />
     </div>
   )
 }
--- src/components/Finder/Finder.jsx
+++ src/components/Finder/Finder.jsx
@@ -1,12 +1,9 @@
 import { useState, useEffect, useCallback, useMemo } from "react";
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Unstable_Grid2';
-import Paper from '@mui/material/Paper';
+ 
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline'
-
-import Avatar from '@mui/material/Avatar';
+ 
 import ScreenSearchDesktopIcon from '@mui/icons-material/ScreenSearchDesktop';
 import Typography from '@mui/material/Typography'
 
@@ -21,7 +18,18 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import CardHeader  from '@mui/material/CardHeader';
+ 
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+ 
+import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 
 
 import {WheelSize} from '../../api/WheelSize'
@@ -37,7 +45,7 @@ const Finder = () => {
   const [year,SetYear] = useState()
   const [generations,SetGenerations] = useState([])
   const [byModel,SetByModel] = useState({})
-
+  const [genImage,SetGenImage] = useState([])
 
   const [modifications, SetModifications] = useState([])
   const [modification, SetModification] = useState()
@@ -63,12 +71,12 @@ const Finder = () => {
   async function fetchModification(make,model,year) {
     const allModification = await WheelSize.fetchModification(make,model,year);
     SetModifications(allModification);
+ 
   }
 
   async function fetchByModel(make,model,year,modification) {
     const allByModels = await WheelSize.fetchByModel(make,model,year,modification)
     SetByModel(allByModels)
-    recorrer()
   }
 
 
@@ -96,24 +104,38 @@ const Finder = () => {
   const handleModifications = (event) =>{
     SetModification(event.target.value);
     fetchByModel(make,model,year,event.target.value) 
+    //recursiveJsonParser(byModel);
 
   }
 
-  const recorrer = () => {
- 
+  function recursiveJsonParser(objeto) {
+    const objResp=new Array();
+      for (var propiedad in objeto) {
+        console.log(propiedad);
+        if (objeto.hasOwnProperty(propiedad)) {
+          if (typeof objeto[propiedad] == "object") {
+            recursiveJsonParser(objeto[propiedad]);
+          } else {
+           //console.log(propiedad + " : " + objeto[propiedad]);
+            if (propiedad == "generation") {
+            // objResp[propiedad] = objeto[propiedad];
+             objResp.push({image:objeto[propiedad]});
+             SetGenerations(objeto[propiedad]);
+             //console.log(generations);
+          }
+        }
+      
+      }
      
-    let claves = Object.keys(byModel); 
-    console.log(byModel[1])
-   /* for(let i=0; i< claves.length; i++)
-    {
-      let clave = claves[i];
-      console.log(clave);
-      console.log(byModel[clave]);
     }
-    //console.log(Object.values(byModel));
-
-*/
+  //  SetGenImage(objResp)
+   // console.log(genImage)
   }
+
+  useEffect(() => {
+    fetchAllMakes();
+ 
+  }, []);
 
 
   return (
@@ -189,30 +211,91 @@ const Finder = () => {
         </FormControl>
 
 
+      <div>
+ 
 
-        
+      {byModel.length &&
+        byModel.map((m) => (
+         
 
-      
           <Card sx={{ maxWidth: 345 }}>
+          <CardHeader
+        
+           
+            title={m.generation.bodies[0].name}
+            subheader={m.generation.bodies[0].slug}
+          />
           <CardMedia
-            sx={{ height: 140 }}
-            image='{g.data.bodies.image}'
-            title="green iguana"
+            component="img"
+            height="194"
+            image={m.generation.bodies[0].image}
+            alt={m.generation.bodies[0].name}
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
             <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica
+
+               <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <LocalGasStationIcon />
+ 
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Tipo Combustible" secondary={m.engine.fuel} />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <SettingsApplicationsIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Tipo motor" secondary={m.engine.type}/>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <BeachAccessIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                  
+                      <ListItemText  primary="Motor"  
+                      secondary={
+                        <>
+                          <Typography
+                            sx={{ display: 'inline' }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                          >
+                           PS: {m.engine.power.PS}
+                           KW:{m.engine.power.KW}
+                           HP :{m.engine.power.HP}
+                          </Typography>
+                          
+                        </>
+                      } />
+           
+                  </ListItem>
+                </List>
             </Typography>
           </CardContent>
-          <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
+          <CardActions disableSpacing>
+           
+         
           </CardActions>
+         
         </Card>
+
+
+
+        ))
+      }
+      </div>
+        
+
+
+      
       
       
      
